2e7e739553c380595f1a833b3226f286119484fa
Rasaf Ibrahim <69809655+Rasaf-Ibrahim@users.noreply.github.com>
2021-03-21 19:26:53 +0600

    updated

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  // mode defaults to 'production' if not set
+  mode: mode,
+
+  // entry not required if using `src/index.js` default
+  // output not required if using `dist/main.js` default
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          // without additional settings, this will reference .babelrc
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+
+  // required if using webpack-dev-server
+  devServer: {
+    contentBase: "./dist",
+  },
+};
