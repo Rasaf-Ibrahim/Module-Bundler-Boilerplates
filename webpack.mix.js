229ee334a7e3aaa2c18f8087297633ce001cf2dd
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-03-27 23:27:38 +0600

    updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -41,3 +41,32 @@ mix
 
 
   //Warning: If you remove browerSync configuration and it's related package by 'npm remove', then this program will not work. 
+
+
+  
+
+
+
+
+
+  /*
+
+
+
+ ***** Reduced js file *****
+
+Usually, in development mode (npm run dev), the file size will be about 330kb. And in the productin mode (npm run prod), the file should be under 100kb. 
+
+
+
+***** Only use the following rule if you don't get a reduce version in the productin mode *****
+
+But if you don't get a reduce file size in the production mode, download vue.min.js and put it in the 'src/js' folder. Then config this file by adding 
+
+ .js('./src/js/vue.min.js', 'dist/app.js') 
+
+ We are outputtin all js file in 'app.js' and 'vue.min.js' is already a reduce version, so, that will help to reduce file size. 
+
+
+
+  */
